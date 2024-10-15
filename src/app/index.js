827c1e69824b93fca9e084b8509e6bac6d16ce5f
Jonathan-Albuquerque-dev <144286666+Jonathan-Albuquerque-dev@users.noreply.github.com>
2024-10-15 04:56:31 -0300

    testando git

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>primeiro app</Text>
+      <Text>Testando o github</Text>
       <StatusBar style="auto" />
     </View>
   );
